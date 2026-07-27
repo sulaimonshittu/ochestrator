332d0cceea9e4f21194b53d106513f64eac9622e
sulaimon <sulele04@gmail.com>
2026-07-27 21:42:09 +0100

    - scaffold

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module ochestrator
 
 go 1.26
+
+require (
+	github.com/docker/go-connections v0.8.1 // indirect
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,64 @@ package main
 
 import (
 	"fmt"
+	"ochestrator/manager"
+	"ochestrator/node"
+	"ochestrator/task"
+	"ochestrator/worker"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
 	}
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+	w := worker.Worker{
+		Name:  "worker-1",
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task)}
+
+	fmt.Printf("worker: %v\n", w)
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDB:  map[string][]*task.Task{},
+		EventDB: map[string][]*task.TaskEvent{},
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
 }
