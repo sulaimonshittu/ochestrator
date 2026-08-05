d3329e976b26fa7d4af02ad03212c8559b1a0e5d
sulaimon <sulele04@gmail.com>
2026-08-05 21:05:33 +0100

    - feat: implmentation of the manager and worker component

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ochestrator
+
+A lightweight orchestrator that provides automation for deploying, scaling, and otherwise managing docker containers.
+
+## ⚙️ Installation
+
+1. *Clone the repo:*
+
+```bash
+git clone https://github.com/sulaimonshittu/ochestrator.git
+```
+
+2. *Install dependencies:*
+
+```bash
+go mod tidy
+```
+
+## Components
+
+- TASK
+- MANAGER
+- WORKER
--- echo/main.go
+++ echo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -16,43 +17,52 @@ type Message struct {
 	Msg string
 }
 
+type Config struct {
+	addr string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	m := Message{}
+	err := d.Decode(&m)
+	if err != nil {
+		json.NewEncoder(w).Encode(errors.New("Unable to decode request body"))
+		return
+	}
+	log.Printf("Received message: %v\n", m.Msg)
+
+	json.NewEncoder(w).Encode(m)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Health check called\n")
+	w.Write([]byte("OK"))
+}
+
+func healthFail(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Health check failed")
+	w.WriteHeader(http.StatusInternalServerError)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
 func main() {
 	r := chi.NewRouter()
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		d := json.NewDecoder(r.Body)
-		m := Message{}
-		err := d.Decode(&m)
-		if err != nil {
-			json.NewEncoder(w).Encode(errors.New("Unable to decode request body"))
-			return
-		}
-		log.Printf("Received message: %v\n", m.Msg)
 
-		json.NewEncoder(w).Encode(m)
-	})
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Health check called")
-		w.Write([]byte("OK"))
-	})
-	r.Get("/healthfail", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Health check failed")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-	})
+	r.Post("/", home)
+	r.Get("/health", health)
+	r.Get("/healthfail", healthFail)
 
-	srv := &http.Server{
-		Addr:    "0.0.0.0:7777",
-		Handler: r,
-	}
+	cfg := Config{}
+	flag.StringVar(&cfg.addr, "addr", ":8080", "Server listening address")
+	flag.Parse()
 
 	go func() {
 		log.Println("Listening on http://localhost:7777")
-		if err := srv.ListenAndServe(); err != nil {
+		if err := http.ListenAndServe(cfg.addr, r); err != nil {
 			log.Println(err)
 		}
 	}()
 
-	// Setup handler for graceful shutdown
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGKILL, syscall.SIGTERM)
 	<-c
